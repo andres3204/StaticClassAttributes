b981a2fc98f7ef3df623a49bbe20454769c1da56
Andres Enrique Jiménez Mogollón <andresejimenez@hotmail.com>
2022-06-05 12:38:58 -0500

    Add project files.

---
+++ StaticClassAttributes/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace StaticClassAttributes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Song lucidDreams = new Song(" Lucid Dreams", " Juice World", 500);
+            Console.WriteLine(Song.songCount);
+            Song lonely = new Song(" Lonely", " Justin Bieber", 300);
+            Console.WriteLine(lonely.GetSongCount());
+
+            Console.ReadLine();
+        }
+    }
+}
